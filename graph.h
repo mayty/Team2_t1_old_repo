3fe997263cfc5be497b4514249fe42c7bf9bae00
thekirjava <thekirjava@yandex.by>
2020-11-10 00:18:31 +0300

    Adjacency list added

--- graph.h
+++ graph.h
@@ -1,11 +1,28 @@
 #pragma once
+#include <vector>
+#include <list>
 #include "SDL_window.h"
+
 class Graph { // class for working with graphs
 private:
+    struct Vertex {
+        struct Edge {
+            size_t to;
+            double length;
+        };
+        struct Coordinates {
+            double x;
+            double y;
+        };
+        size_t originalIdx;
+        std::list<Edge> edges;
+        Coordinates coordinates;
+    };
+    std::vector<Vertex> adjacencyList;
 public:
-	Graph(const std::string& filename); // creates graph with points in circular layout from file with json data
-	void Draw(SDL_Window& window); // draws current graph
-	void ApplyForce(); // applyes forces to verteces
-	~Graph();
+    explicit Graph(const std::string& filename); // creates graph with points in circular layout from file with json data
+    void Draw(SDL_Window& window) const; // draws current graph
+    void ApplyForce(); // applyes forces to verteces
+    ~Graph();
 };
 
