03abe01abe112e449d81634536cad6184513be1b
mAyty <pik.paval@gmail.com>
2020-11-11 20:43:28 +0300

    added scaling and moving to window

--- SDL_window.cpp
+++ SDL_window.cpp
@@ -1,6 +1,8 @@
 #include "SDL_window.h"
 #include <stdexcept>
 
+#include <iostream>
+
 SdlWindow::SdlWindow(const std::string& name, size_t width, size_t height) {
 	window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
 	if (!window) {
@@ -13,16 +15,16 @@ SdlWindow::SdlWindow(const std::string& name, size_t width, size_t height) {
 	}
 }
 
-void SdlWindow::DrawLine(size_t x0, size_t y0, size_t x1, size_t y1) {
-	SDL_RenderDrawLine(renderer, x0, y0, x1, y1);
+void SdlWindow::DrawLine(int x0, int y0, int x1, int y1) {
+	SDL_RenderDrawLine(renderer, offset_x + x0 * scale, offset_y + y0 * scale, offset_x + x1 * scale, offset_y + y1 * scale);
 }
 
-void SdlWindow::DrawRectangle(size_t x0, size_t y0, size_t x1, size_t y1) {
+void SdlWindow::DrawRectangle(int x0, int y0, int x1, int y1) {
 	SDL_Rect rect;
-	rect.x = x0;
-	rect.y = y0;
-	rect.h = y1 - y0;
-	rect.w = x1 - x0;
+	rect.x = offset_x + x0 * scale;
+	rect.y = offset_y + y0 * scale;
+	rect.h = (y1 - y0) * scale;
+	rect.w = (x1 - x0) * scale;
 	SDL_RenderDrawRect(renderer, &rect);
 }
 
@@ -41,8 +43,35 @@ void SdlWindow::Update() {
 bool SdlWindow::HasCloseRequest() {
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT) {
+		switch (event.type) {
+		case SDL_QUIT:
 			return true;
+		case SDL_MOUSEWHEEL:
+			if (event.wheel.y < 0) {
+				zoomIn();
+			}
+			else {
+				zoomOut();
+			}
+			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.scancode) {
+			case SDL_SCANCODE_ESCAPE:
+				return true;
+			case SDL_SCANCODE_UP:
+				moveUp();
+				break;
+			case SDL_SCANCODE_DOWN:
+				moveDown();
+				break;
+			case SDL_SCANCODE_LEFT:
+				moveLeft();
+				break;
+			case SDL_SCANCODE_RIGHT:
+				moveRight();
+				break;
+			}
+			break;
 		}
 	}
 	return false;
@@ -55,4 +84,38 @@ SdlWindow::~SdlWindow() {
 	if (window) {
 		SDL_DestroyWindow(window);
 	}
+}
+
+void SdlWindow::moveUp()
+{
+	offset_y += 5;
+}
+
+void SdlWindow::moveDown()
+{
+	offset_y -= 5;
+}
+
+void SdlWindow::moveLeft()
+{
+	offset_x += 5;
+}
+
+void SdlWindow::moveRight()
+{
+	offset_x -= 5;
+}
+
+void SdlWindow::zoomIn()
+{
+	scale -= 0.1;
+	if (scale < 0.1)
+	{
+		scale = 0.1;
+	}
+}
+
+void SdlWindow::zoomOut()
+{
+	scale += 0.1;
 }
--- SDL_window.h
+++ SDL_window.h
@@ -5,14 +5,24 @@ class SdlWindow { // window class containing all methods for drawing
 private:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	int offset_x = 0;
+	int offset_y = 0;
+	double scale = 1.0;
 public:
 	SdlWindow(const std::string& name, size_t width = 800, size_t height = 600);
-	void DrawLine(size_t x0, size_t y0, size_t x1, size_t y1);
-	void DrawRectangle(size_t x0, size_t y0, size_t x1, size_t y1);
+	void DrawLine(int x0, int y0, int x1, int y1);
+	void DrawRectangle(int x0, int y0, int x1, int y1);
 	void SetDrawColor(unsigned char r, unsigned char g, unsigned char b);
 	void Clear();
 	void Update();
 	bool HasCloseRequest();
 	~SdlWindow();
+private:
+	void moveUp();
+	void moveDown();
+	void moveLeft();
+	void moveRight();
+	void zoomIn();
+	void zoomOut();
 };
 
