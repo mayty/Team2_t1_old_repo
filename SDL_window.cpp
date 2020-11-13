e15291df97dcc1e0c667d68b2c3ee5c89145b49d
mAyty <pik.paval@gmail.com>
2020-11-14 00:11:26 +0300

    Implemented automatic graph preview scaling and positioning

--- SDL_window.cpp
+++ SDL_window.cpp
@@ -1,9 +1,11 @@
 #include "SDL_window.h"
 #include <stdexcept>
-
 #include <iostream>
 
 SdlWindow::SdlWindow(const std::string& name, size_t width, size_t height) {
+	this->width = width;
+	this->height = height;
+	has_target = false;
 	window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
 	if (!window) {
 		throw std::runtime_error{ SDL_GetError() };
@@ -17,6 +19,7 @@ SdlWindow::SdlWindow(const std::string& name, size_t width, size_t height) {
 
 void SdlWindow::DrawLine(int x0, int y0, int x1, int y1) {
 	SDL_RenderDrawLine(renderer, offset_x + x0 * scale, offset_y + y0 * scale, offset_x + x1 * scale, offset_y + y1 * scale);
+	updateTarget(x0, y0, x1, y1);
 }
 
 void SdlWindow::DrawRectangle(int x0, int y0, int x1, int y1) {
@@ -25,7 +28,15 @@ void SdlWindow::DrawRectangle(int x0, int y0, int x1, int y1) {
 	rect.y = offset_y + y0 * scale;
 	rect.h = (y1 - y0) * scale;
 	rect.w = (x1 - x0) * scale;
-	SDL_RenderDrawRect(renderer, &rect);
+	if (std::min(rect.h, rect.w) > 1)
+	{
+		SDL_RenderDrawRect(renderer, &rect);
+	}
+	else
+	{
+		SDL_RenderDrawPoint(renderer, rect.x, rect.y);
+	}
+	updateTarget(x0, y0, x1, y1);
 }
 
 void SdlWindow::SetDrawColor(unsigned char r, unsigned char g, unsigned char b) {
@@ -38,6 +49,15 @@ void SdlWindow::Clear() {
 
 void SdlWindow::Update() {
 	SDL_RenderPresent(renderer);
+	if (has_target)
+	{
+		scale = std::min(width / ((target_max_x - target_min_x)), height / ((target_max_y - target_min_y)));
+		offset_x = -target_min_x * scale;
+		offset_x += (width - (target_max_x * scale + offset_x)) / 2;
+		offset_y = -target_min_y * scale;
+		offset_y += ((height - (target_max_y * scale + offset_y)) / 2);
+	}
+	has_target = false;
 }
 
 bool SdlWindow::HasCloseRequest() {
@@ -46,32 +66,11 @@ bool SdlWindow::HasCloseRequest() {
 		switch (event.type) {
 		case SDL_QUIT:
 			return true;
-		case SDL_MOUSEWHEEL:
-			if (event.wheel.y < 0) {
-				zoomIn();
-			}
-			else {
-				zoomOut();
-			}
-			break;
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.scancode) {
 			case SDL_SCANCODE_ESCAPE:
 				return true;
-			case SDL_SCANCODE_UP:
-				moveUp();
-				break;
-			case SDL_SCANCODE_DOWN:
-				moveDown();
-				break;
-			case SDL_SCANCODE_LEFT:
-				moveLeft();
-				break;
-			case SDL_SCANCODE_RIGHT:
-				moveRight();
-				break;
 			}
-			break;
 		}
 	}
 	return false;
@@ -86,36 +85,30 @@ SdlWindow::~SdlWindow() {
 	}
 }
 
-void SdlWindow::moveUp()
+void SdlWindow::updateTarget(int minX, int minY, int maxX, int maxY)
 {
-	offset_y += 5;
-}
-
-void SdlWindow::moveDown()
-{
-	offset_y -= 5;
-}
-
-void SdlWindow::moveLeft()
-{
-	offset_x += 5;
-}
-
-void SdlWindow::moveRight()
-{
-	offset_x -= 5;
-}
-
-void SdlWindow::zoomIn()
-{
-	scale -= 0.1;
-	if (scale < 0.1)
+	if (!has_target)
 	{
-		scale = 0.1;
+		target_min_x = minX;
+		target_max_x = maxX;
+		target_min_y = minY;
+		target_max_y = maxY;
 	}
-}
-
-void SdlWindow::zoomOut()
-{
-	scale += 0.1;
+	has_target = true;
+	if (target_min_x > minX)
+	{
+		target_min_x = minX;
+	}
+	if (target_max_x < maxX)
+	{
+		target_max_x = maxX;
+	}
+	if (target_min_y > minY)
+	{
+		target_min_y = minY;
+	}
+	if (target_max_y < maxY)
+	{
+		target_max_y = maxY;
+	}
 }
--- SDL_window.h
+++ SDL_window.h
@@ -5,9 +5,16 @@ class SdlWindow { // window class containing all methods for drawing
 private:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	int width;
+	int height;
 	int offset_x = 0;
 	int offset_y = 0;
 	double scale = 1.0;
+	double target_max_x;
+	double target_min_x;
+	double target_max_y;
+	double target_min_y;
+	bool has_target;
 public:
 	SdlWindow(const std::string& name, size_t width = 800, size_t height = 600);
 	void DrawLine(int x0, int y0, int x1, int y1);
@@ -18,11 +25,6 @@ public:
 	bool HasCloseRequest();
 	~SdlWindow();
 private:
-	void moveUp();
-	void moveDown();
-	void moveLeft();
-	void moveRight();
-	void zoomIn();
-	void zoomOut();
+	void updateTarget(int minX, int minY, int maxX, int maxY);
 };
 
