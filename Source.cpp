f7a4eed910efb83bb88a93e54491816c30956c07
mAyty <pik.paval@gmail.com>
2020-11-14 00:09:06 +0300

    updated main

--- Source.cpp
+++ Source.cpp
@@ -6,31 +6,41 @@
 #include <thread>
 
 constexpr int frameTime = 33;
+constexpr double stableThreshold = 20.0;
 
 int main(int argC, char** argV) {
 	SdlManager manager{};
 	SdlWindow window{"graph demo", 800, 600};
-	Graph demo_graph{ "JSON_test_files/big_graph.json" };
-	bool to_exit = false;
-	auto last_update_time = std::chrono::high_resolution_clock::now();
-	std::thread graphCalcThread{ [&demo_graph, &to_exit]() {
-		double change = 100.0;
-		while (!to_exit && change > 20.0) {
-			change = demo_graph.ApplyForce();
+	std::string filename;
+	if (argC < 2)
+	{
+		filename = "JSON_test_files/big_graph.json";
+	}
+	else
+	{
+		filename = argV[1];
+	}
+	Graph demoGraph{ filename };
+	bool toExit = false;
+	auto lastUpdateTime = std::chrono::high_resolution_clock::now();
+	std::thread graphCalcThread{ [&demoGraph, &toExit]() {
+		double change = stableThreshold;
+		while (!toExit && change >= stableThreshold) {
+			change = demoGraph.ApplyForce();
 		} 
 	} 
 	};
 
-	while (!(to_exit = window.HasCloseRequest())) {
+	while (!(toExit = window.HasCloseRequest())) {
 		window.SetDrawColor(0, 0, 0);
 		window.Clear();
-		demo_graph.Draw(window);
+		demoGraph.Draw(window);
 
-		auto current_time = std::chrono::high_resolution_clock::now();
-		if (current_time - last_update_time < std::chrono::milliseconds{ frameTime }) {
-			std::this_thread::sleep_for(std::chrono::milliseconds{ frameTime } - (current_time - last_update_time));
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		if (currentTime - lastUpdateTime < std::chrono::milliseconds{ frameTime }) {
+			std::this_thread::sleep_for(std::chrono::milliseconds{ frameTime } - (currentTime - lastUpdateTime));
 		}
-		last_update_time = std::chrono::high_resolution_clock::now();
+		lastUpdateTime = std::chrono::high_resolution_clock::now();
 		window.Update();
 	}
 
